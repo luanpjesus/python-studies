3259e876c0844f84ee98c922f69ffc1514c4ef95
luan.pjesus <luan.pjesus@telefonica.com>
2026-07-24 18:26:47 -0300

    READ method added

---
+++ api/flask_api.py
@@ -0,0 +1,84 @@
+from flask import Flask, request, jsonify
+from models.task import Task
+app = Flask(__name__)
+
+
+# @app.route("/")
+# def hello_world():
+#     return "Hello world"
+
+
+
+# @app.route("/about")
+# def about():
+#     return "Pagina Sobre!"
+
+
+
+tasks = []
+task_id_control = 1 
+@app.route('/tasks', methods=['POST'])
+def create_task():
+
+    global task_id_control
+    data = request.get_json()
+    
+    new_task = Task(
+        id=task_id_control,title=data["title"],description= data.get("description", "")
+        
+        )
+    task_id_control += 1
+    tasks.append(new_task)
+    print(tasks)
+    return jsonify({"message": "Nova tarefa criada com sucesso!"})
+
+
+
+
+
+
+@app.route('/tasks', methods =['GET'])
+def get_tasks():
+    task_list = [task.to_dict() for task in tasks]
+    output = {
+        "task": task_list,
+        "total_task": 0
+}   
+    return jsonify(output)
+    
+    
+               
+            
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
+#CRUD
+#Create, Read, Update and Delete
+#Tarefa:    
+    
+#REST - REPRESENTATIONAL STATE TRANSFER
+#ESTILO DE ARCH PARA API
+
+
+
+#RESTFUL
+#QUANDO A API RESPEITA TODOS OS PRINCIPIOS DO REST
+
+
+
+#  - HTTP
+#        *GET
+#        *POST
+#        *DELETE
+#        *PUT
+#        *PATCH
+
+
+
+
+ 
+
+
+    
+    
