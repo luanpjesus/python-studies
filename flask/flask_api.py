8ee06ebafdabc5ad4de8383352f653a6c36c850b
luan.pjesus <luan.pjesus@telefonica.com>
2026-07-23 21:17:22 -0300

    codigo arrumado, testar no postman

--- flask/flask_api.py
+++ flask/flask_api.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from models.task import Task
 app = Flask(__name__)
 
@@ -19,14 +19,18 @@ tasks = []
 task_id_control = 1 
 @app.route('/tasks', methods=['POST'])
 def create_task():
+
+    global task_id_control
     data = request.get_json()
     
     new_task = Task(
-        id=task_id_control,title=data.['title'],description= data.get()
+        id=task_id_control,title=data["title"],description= data.get("description", "")
         
         )
     task_id_control += 1
-    return "Teste"
+    tasks.append(new_task)
+    print(tasks)
+    return jsonify({"message": "Nova tarefa criada com sucesso!"})
 
 
 
