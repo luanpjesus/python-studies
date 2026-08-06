f73662bee8283b7e87a0e880409be014a6d19236
Luan <luanpjesus@gmail.com>
2026-08-06 17:26:53 -0300

    tests add

--- api/flask_api.py
+++ api/flask_api.py
@@ -12,102 +12,90 @@ app = Flask(__name__)
 #     return "Pagina Sobre!"
 
 tasks: list[Task] = []
-task_id_control = 1 
+task_id_control = 1
 
-@app.route('/tasks', methods=['POST'])
+
+@app.route("/tasks", methods=["POST"])
 def create_task():
 
     global task_id_control
     data = request.get_json()
-    
+
     new_task = Task(
-        id=task_id_control,title=data["title"],description= data.get("description", "")
-        
-        )
+        id=task_id_control, title=data["title"], description=data.get("description", "")
+    )
     task_id_control += 1
     tasks.append(new_task)
     print(new_task.to_dict())
-    return jsonify({"message": "Nova tarefa criada com sucesso!"})
+    return jsonify({"message": "Nova tarefa criada com sucesso!", "id": new_task.id})
 
 
-
-
-@app.route('/tasks', methods=['GET'])
+@app.route("/tasks", methods=["GET"])
 def get_task():
-    
+
     task_list = [task.to_dict() for task in tasks]
-    
-    
-    output = {
-        "task": task_list,
-        "total_tasks": len(task_list)
-    } 
-    
+
+    output = {"task": task_list, "total_tasks": len(task_list)}
+
     return jsonify(output), 200
 
-@app.route('/tasks/<int:id>', methods=['GET'])
+
+@app.route("/tasks/<int:id>", methods=["GET"])
 def get_tasks(id):
-        
+
     for t in tasks:
         if t.id == id:
             return jsonify(t.to_dict())
-    return jsonify({"message" : "Nao foi possivel encontrar a atividade"}), 404    
-    
-    
-@app.route('/tasks/<int:id>', methods=['PUT']) 
+    return jsonify({"message": "Nao foi possivel encontrar a atividade"}), 404
+
+
+@app.route("/tasks/<int:id>", methods=["PUT"])
 def update_task(id):
     task = None
     for t in tasks:
         if t.id == id:
             task = t
-    print(task)        
-    if task == None:
-        return jsonify({"message":"Nao possivel encontrar a atividade"}), 404    
-    data = request.get_json()
-    task.title = data['title'] 
-    task.description = data['description'] 
-    task.completed = data['completed']
     print(task)
-    return jsonify({"message":"Tarefa atualizada com sucesso"})
-       
-    
-@app.route('/tasks/<int:id>', methods=['DELETE'])
+    if task == None:
+        return jsonify({"message": "Nao possivel encontrar a atividade"}), 404
+    data = request.get_json()
+    task.title = data["title"]
+    task.description = data["description"]
+    task.completed = data["completed"]
+    print(task)
+    return jsonify({"message": "Tarefa atualizada com sucesso"})
+
+
+@app.route("/tasks/<int:id>", methods=["DELETE"])
 def delete_task(id):
     task = None
     for t in tasks:
         if t.id == id:
             task = t
-    print(task)        
+    print(task)
     if not task:
         return jsonify({"message": "Nao foi possivel encontrar a atividade"}), 404
     tasks.remove(task)
     return jsonify({"message": "Tarefa deletada com sucesso"})
 
 
-
 # @app.route('/user/')
 # def get_update():
-    
 
-    
-         
-            
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
-#CRUD
-#Create, Read, Update and Delete
-#Tarefa:    
-    
-#REST - REPRESENTATIONAL STATE TRANSFER
-#ESTILO DE ARCH PARA API
+
+# CRUD
+# Create, Read, Update and Delete
+# Tarefa:
+
+# REST - REPRESENTATIONAL STATE TRANSFER
+# ESTILO DE ARCH PARA API
 
 
-
-#RESTFUL
-#QUANDO A API RESPEITA TODOS OS PRINCIPIOS DO REST
-
+# RESTFUL
+# QUANDO A API RESPEITA TODOS OS PRINCIPIOS DO REST
 
 
 #  - HTTP
@@ -116,12 +104,3 @@ if __name__ == "__main__":
 #        *DELETE
 #        *PUT
 #        *PATCH
-
-
-
-
- 
-
-
-    
-    
---
+++ api/test.py
@@ -0,0 +1,22 @@
+import requests
+
+# CRUD
+BASE_URL = "http://127.0.0.1:5000"
+tasks = []
+
+
+def test_create_task():
+    new_tesk_data = {"title": "Nova tarefa", "description": "Descricao da nova tarefa"}
+    response = requests.post(f"{BASE_URL}/tasks", json=new_tesk_data)
+    assert response.status_code == 200
+    response_json = response.json()
+    assert "message" in response_json
+    assert "id" in response_json
+
+    tasks.append(response_json["id"])
+
+
+# def test_get_tasks():
+#    get_new_task =
+
+#   response = requests.post(f"{BASE_URL}/tasks", json=get_new_task)
